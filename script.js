342f7e857ab7da5bb07c7f7dcc57aa2082abdf8c
Hritik Pandey <120927183+Hkp005@users.noreply.github.com>
2023-10-25 19:02:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,6 +31,54 @@ const chatbox = document.querySelector(".chatbox");
 
 let userMessage;
 
+// Define questions and options
+const questions = [
+    {
+        question: "Hi How can I help you?",
+        options: ["Know your prakruti"],
+      },
+  {
+    question: "What is your body frame like?",
+    options: ["Slim", "Medium", "Heavy"],
+  },
+  {
+    question: "What is your skin type?",
+    options: ["Dry" , "Oily" , "Combination"],
+  },
+  {
+    question: "How is your hair type?",
+    options: ["Straight", "Wavy", "Curly"],
+  },
+  {
+    question: "Do you feel cold or hot more often?",
+    options: [ "Cold" , "Hot"],
+  },
+  {
+    question: "How do you respond to changes in weather or climate?",
+    options: ["Easily affected", "Moderate effect", "Less affected"],
+  },
+  {
+    question: "How do you typically react to stress or challenges?",
+    options: ["Anxious or worried", "Confident and determined", "Calm and composed"],
+  },
+  // Add more questions and options as needed
+];
+
+let currentQuestion = 0;
+
+// Create a chat message with options
+const createQuestionMessage = (question) => {
+  const questionMessage = createChatLi(question.question, "incoming");
+  question.options.forEach((option) => {
+    const optionButton = document.createElement("button");
+    optionButton.textContent = option;
+    optionButton.classList.add("option-button");
+    optionButton.addEventListener("click", () => handleUserChoice(option));
+    questionMessage.appendChild(optionButton);
+  });
+  chatbox.appendChild(questionMessage);
+};
+
 const createChatLi = (message, className) => {
   const chatLi = document.createElement("li");
   chatLi.classList.add("chat", className);
@@ -42,6 +90,30 @@ const createChatLi = (message, className) => {
   return chatLi;
 };
 
+const handleUserChoice = (choice) => {
+  chatbox.querySelectorAll(".option-button").forEach((button) => {
+    button.style.display = "none"; // Hide option buttons
+  });
+  const userChoiceMessage = createChatLi(`User selected: ${choice}`, "outgoing");
+  chatbox.appendChild(userChoiceMessage);
+  sendBotResponse(choice); // Send the user's choice to the bot
+};
+
+const sendBotResponse = (userChoice) => {
+  // Simulate a bot response based on user choice (replace with your logic)
+  setTimeout(() => {
+    const botResponse = `You selected ${userChoice}`;
+    chatbox.appendChild(createChatLi(botResponse, "incoming"));
+    if (currentQuestion < questions.length - 1) {
+      currentQuestion++; // Move to the next question
+      createQuestionMessage(questions[currentQuestion]);
+    } else {
+      // Chatbot reached the end of questions
+      chatInput.disabled = true; // Disable input after finishing questions
+    }
+  }, 1000); // Simulate a 1-second delay
+};
+
 const handleChat = () => {
   userMessage = chatInput.value.trim();
   if (!userMessage) return;
@@ -50,18 +122,10 @@ const handleChat = () => {
 
   setTimeout(() => {
     chatbox.appendChild(createChatLi("Thinking", "incoming"));
-    sendBotResponse();
+    sendBotResponse(userMessage); // Send the user's message to the bot
   }, 600);
 
   chatInput.value = ""; // Clear the input field
-};
-
-const sendBotResponse = () => {
-  // Simulate a bot response (replace with your bot logic)
-  setTimeout(() => {
-    const botResponse = "This is a bot response. Replace this with your bot logic.";
-    chatbox.appendChild(createChatLi(botResponse, "incoming"));
-  }, 1000); // Simulate a 1-second delay
 };
 
 sendChatBtn.addEventListener("click", handleChat);
@@ -73,3 +137,6 @@ chatInput.addEventListener("keydown", (e) => {
     handleChat(); // Trigger the chat handling function
   }
 });
+
+// Start the chat by displaying the first question
+createQuestionMessage(questions[currentQuestion]);
